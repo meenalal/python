335f2c9d23d4e53bba76f18f1d6e9dbc7b7e10c8
Meenal <meenalalagappan89@gmail.com>
2018-01-31 15:52:06 -0800

    Add a new file

---
+++ guess - Copy.py
@@ -0,0 +1,37 @@
+# Guess the number game
+# Computer will think random number from 1 to 20 and ask you to guess. you only get six guess but it will tell you your guess too low or high
+
+import random
+
+guessesTaken = 0
+
+print('Hello! What is your name?')
+myName = input()
+
+number = random.randint(1, 20)
+print('Well, ' + myName + ', I am thinking of a number between 1 and 20. ')
+
+while guessesTaken < 8:
+    print('Take a guess.') # There are four spaces in front of print.
+    guess = input()
+    guess = int(guess)
+
+    guessesTaken = guessesTaken + 1
+
+    if guess < number:
+        print('Your guess is too low. ') #There are eight spaces in front of print.
+
+    if guess > number:
+        print('Your guess is too high. ')
+
+    if guess == number:
+        break
+
+if guess == number:
+    guessesTaken = str(guessesTaken)
+    print('Good job, ' + myName + '! You guessed my number in ' + guessesTaken + ' guesses!')
+
+if guess != number:
+    number = str(number)
+    print('Nope. The numbern I was thinking of was ' + number)
+    
